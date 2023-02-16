15f1a970199e51fad8232fa8d686d1715cefa7e0
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-02-16 17:31:15 +0200

    Remove example stuff

---
+++ src/main/java/top/offsetmonkey538/egggenerator/EggGenerator.java
@@ -0,0 +1,17 @@
+package top.offsetmonkey538.egggenerator;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EggGenerator implements ModInitializer {
+
+	public static final String MOD_ID = "egg-generator";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
