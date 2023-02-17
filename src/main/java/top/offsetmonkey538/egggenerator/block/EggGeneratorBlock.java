2ee68eb60facb0662cb526f1365d30934d6b6f3b
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-02-17 14:30:33 +0200

    Make a block called 'egg_generator'

---
+++ src/main/java/top/offsetmonkey538/egggenerator/block/EggGeneratorBlock.java
@@ -0,0 +1,10 @@
+package top.offsetmonkey538.egggenerator.block;
+
+import net.minecraft.block.Block;
+
+public class EggGeneratorBlock extends Block {
+
+    public EggGeneratorBlock(Settings settings) {
+        super(settings);
+    }
+}
--- src/main/java/top/offsetmonkey538/egggenerator/block/ModBlocks.java
+++ src/main/java/top/offsetmonkey538/egggenerator/block/ModBlocks.java
@@ -1,7 +1,9 @@
 package top.offsetmonkey538.egggenerator.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -10,6 +12,8 @@ import net.minecraft.util.Identifier;
 import static top.offsetmonkey538.egggenerator.EggGenerator.MOD_ID;
 
 public class ModBlocks {
+
+    public static final EggGeneratorBlock EGG_GENERATOR = registerWithItem("egg_generator", new EggGeneratorBlock(FabricBlockSettings.copyOf(Blocks.HAY_BLOCK)));
 
     private static <T extends Block> T registerWithItem(String name, T block) {
         Registry.register(Registries.ITEM, new Identifier(MOD_ID, name), new BlockItem(block, new FabricItemSettings()));
