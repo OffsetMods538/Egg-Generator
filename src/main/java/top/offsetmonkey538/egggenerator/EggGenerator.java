b58bc107d8909227b3824b93e3f93baf635f2340
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-02-18 21:06:48 +0200

    Add egg generators to the 'Functional Blocks' item group

--- src/main/java/top/offsetmonkey538/egggenerator/EggGenerator.java
+++ src/main/java/top/offsetmonkey538/egggenerator/EggGenerator.java
@@ -2,8 +2,13 @@ package top.offsetmonkey538.egggenerator;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static top.offsetmonkey538.egggenerator.ModBlocks.*;
 
 public class EggGenerator implements ModInitializer {
 
@@ -13,5 +18,12 @@ public class EggGenerator implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModBlocks.initialize();
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> {
+			content.addAfter(Items.BLAST_FURNACE,           TIER_1_EGG_GENERATOR.asItem());
+			content.addAfter(TIER_1_EGG_GENERATOR.asItem(), TIER_2_EGG_GENERATOR.asItem());
+			content.addAfter(TIER_2_EGG_GENERATOR.asItem(), TIER_3_EGG_GENERATOR.asItem());
+			content.addAfter(TIER_3_EGG_GENERATOR.asItem(), TIER_4_EGG_GENERATOR.asItem());
+		});
 	}
 }
