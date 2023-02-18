464de4d63dcdcd8a37dd2b5ad7feb27b89b7c158
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-02-18 18:40:15 +0200

    Make the chicken in a tier 4 egg generator *spin*

--- src/client/java/top/offsetmonkey538/egggenerator/EggGeneratorBlockEntityRenderer.java
+++ src/client/java/top/offsetmonkey538/egggenerator/EggGeneratorBlockEntityRenderer.java
@@ -9,11 +9,14 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.ChickenEntity;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.RotationAxis;
+import net.minecraft.world.World;
 import top.offsetmonkey538.egggenerator.block.entity.AbstractEggGeneratorBlockEntity;
+import top.offsetmonkey538.egggenerator.block.entity.Tier4EggGeneratorBlockEntity;
 
 public class EggGeneratorBlockEntityRenderer implements BlockEntityRenderer<AbstractEggGeneratorBlockEntity> {
     private final EntityRenderDispatcher entityRenderDispatcher;
     private ChickenEntity chicken;
+    private World world;
 
     public EggGeneratorBlockEntityRenderer(BlockEntityRendererFactory.Context context) {
         this.entityRenderDispatcher = context.getEntityRenderDispatcher();
@@ -22,17 +25,23 @@ public class EggGeneratorBlockEntityRenderer implements BlockEntityRenderer<Abst
     @Override
     @SuppressWarnings("DataFlowIssue")
     public void render(AbstractEggGeneratorBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        if (this.world == null) this.world = entity.getWorld();
         if (this.chicken == null) {
-            this.chicken = EntityType.CHICKEN.create(entity.getWorld());
+            this.chicken = EntityType.CHICKEN.create(world);
             this.chicken.setAiDisabled(true);
         }
+
+        float rotation = 0;
+        boolean isTier4 = entity instanceof Tier4EggGeneratorBlockEntity;
+        if (isTier4) rotation = (entity.getWorld().getTime() + tickDelta) * 64;
+
         matrices.push();
 
         matrices.scale(0.5f, 0.5f, 0.5f);
         matrices.translate(0.75f, 0.63f, 1f);
-        matrices.multiply(RotationAxis.NEGATIVE_Y.rotationDegrees(entity.getCachedState().get(Properties.HORIZONTAL_FACING).asRotation()));
+        matrices.multiply(RotationAxis.NEGATIVE_Y.rotationDegrees(entity.getCachedState().get(Properties.HORIZONTAL_FACING).asRotation() + rotation));
 
-        this.entityRenderDispatcher.render(chicken, 0, 0, 0, 0, 0, matrices, vertexConsumers, light);
+        this.entityRenderDispatcher.render(chicken, 0, 0, 0, 0, isTier4 ? tickDelta : 0, matrices, vertexConsumers, light);
 
         matrices.pop();
     }
