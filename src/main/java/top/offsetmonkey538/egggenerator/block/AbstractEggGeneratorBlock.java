9d94d1646f43e1fc2cd654aebecc495e34063c96
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-02-18 18:16:40 +0200

    Make egg generators face a certain direction

--- src/main/java/top/offsetmonkey538/egggenerator/block/AbstractEggGeneratorBlock.java
+++ src/main/java/top/offsetmonkey538/egggenerator/block/AbstractEggGeneratorBlock.java
@@ -1,18 +1,28 @@
 package top.offsetmonkey538.egggenerator.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import top.offsetmonkey538.egggenerator.block.entity.AbstractEggGeneratorBlockEntity;
 
 public abstract class AbstractEggGeneratorBlock extends BlockWithEntity {
+    public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
 
     public AbstractEggGeneratorBlock(Settings settings) {
         super(settings.nonOpaque());
+        setDefaultState(getDefaultState().with(FACING, Direction.NORTH));
     }
 
     @Override
@@ -25,5 +35,29 @@ public abstract class AbstractEggGeneratorBlock extends BlockWithEntity {
     @Override
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+        BlockState state = super.getPlacementState(context);
+        if (state == null) return null;
+        return state.with(FACING, context.getPlayerFacing().getOpposite());
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
     }
 }
